b93aac2f365547ed854dccfba0ed3d899c02b77b
Christopher Kaster <ikasoki@gmail.com>
2012-03-18 16:52:59 +0100

    added wrapper for Rectangle with RyVector support

---
+++ src/net/robertboehm/rygal/RyRectangle.hx
@@ -0,0 +1,22 @@
+// Copyright (C) 2012 Christopher Kaster
+// This file is part of Rygal.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with Rygal. If not, see: <http://www.gnu.org/licenses/>.
+
+package net.robertboehm.rygal;
+import nme.geom.Rectangle;
+
+/**
+ * ...
+ * @author Christopher Kaster
+ */
+
+class RyRectangle extends Rectangle {
+	
+	public var position(getPosition, null):RyVector;
+	
+	private function getPosition():RyVector {
+		return new RyVector(this.x, this.y);
+	}
+}
