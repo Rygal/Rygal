926a4a2a5ee4e46ec05e123e96d26bf53c98c2a8
Robert Böhm <robert.boehm94@gmail.com>
2012-04-20 16:21:22 +0200

    Added sounds. Still needs jukebox for #13.

---
+++ net/robertboehm/rygal/audio/RySoundEvent.hx
@@ -0,0 +1,24 @@
+// Copyright (C) 2012 Robert Böhm
+// This file is part of Rygal.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with Rygal. If not, see: <http://www.gnu.org/licenses/>.
+
+
+package net.robertboehm.rygal.audio;
+import nme.events.Event;
+
+/**
+ * ...
+ * @author Robert Böhm
+ */
+
+class RySoundEvent extends Event {
+	
+	public static var SOUND_COMPLETE:String = "soundComplete";
+
+	public function new(type:String) {
+		super(type);
+	}
+	
+}
--- net/robertboehm/rygal/util/RyStorage.hx
+++ net/robertboehm/rygal/util/RyStorage.hx
@@ -1,5 +1,15 @@
+// Copyright (C) 2012 Robert Böhm
+// This file is part of Rygal.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with Rygal. If not, see: <http://www.gnu.org/licenses/>.
+
+
 package net.robertboehm.rygal.util;
+
+#if !js
 import nme.net.SharedObject;
+#end
 
 /**
  * ...
@@ -7,8 +17,29 @@ import nme.net.SharedObject;
  */
 
 class RyStorage {
+	#if js
+	
+	public static function canStore():Bool { return false; }
+	
+	public function new(name:String) {}
+	
+	public function clear():Void {}
+	
+	public function isset(key:String):Bool { return false; }
+	
+	public function unset(key:String):Void {}
+	
+	public function put(key:String, data:Dynamic):Void {}
+	
+	public function get(key:String, defaultData:Dynamic = null):Dynamic { return defaultData; }
+	
+	#else
 	
 	private var object:SharedObject;
+	
+	public function canStore():Bool {
+		return true;
+	}
 	
 	public function new(name:String) {
 		object = SharedObject.getLocal(name);
@@ -40,5 +71,7 @@ class RyStorage {
 			return defaultData;
 		}
 	}
+	
+	#end
 	
 }
